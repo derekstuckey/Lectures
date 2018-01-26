1127b6601e1121bf72895fa21d03fad9296e36d1
Derek Stuckey <djs37549@live.missouristate.edu>
2018-01-26 13:09:57 -0600

    sorting dictionaries

---
+++ csc131/dictionaries.py
@@ -0,0 +1,6 @@
+def get_personal_data() -> dict:
+    """
+    :return: Returns a dictionary of personal information
+    """
+    personal_data = {"name": "Derek", "a_role": "Student"}
+    return personal_data
---
+++ lect2018-26-01.py
@@ -0,0 +1,13 @@
+from csc131 import dictionaries
+
+
+def main():
+    print("Main")
+    d = dictionaries.get_personal_data()
+    print(d)
+    for key in sorted(d.keys()):
+        print("%s: %s" %(key, d[key]))
+#prints the key then the value of that key^ in alphabetical order
+
+if __name__ == '__main__':
+    main()
