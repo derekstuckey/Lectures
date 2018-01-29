38af7e05104316b64b5fcf82ee30b8a470a76221
Derek Stuckey <djs37549@live.missouristate.edu>
2018-01-29 13:10:58 -0600

    making dictionaries and translating strings

--- csc131/dictionaries.py
+++ csc131/dictionaries.py
@@ -1,6 +1,30 @@
+import string
+
 def get_personal_data() -> dict:
     """
     :return: Returns a dictionary of personal information
     """
     personal_data = {"name": "Derek", "a_role": "Student"}
     return personal_data
+
+
+def main() -> int:
+    default_dict = dict()
+    print(default_dict)         #will always give back a blank dict
+    initialized_dict = dict([('name', 'Derek'), ('a_role', 'joker')])       #dict constructor
+    print(initialized_dict)
+    simple_init_dict = dict(name='Derek', a_role='student')                 #dict constructor
+    print(simple_init_dict)
+    simple_init_dict['a_role'] = "joker" #changes value associated with key
+    print(simple_init_dict)
+    my_comprehension = {x: x**2 for x in range(5)}      #dict comprehension
+    print(my_comprehension)
+
+    s = "little,".translate({ord(i): None for i in string.punctuation})         #print our the string withought the punctuation
+    print(s)
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
