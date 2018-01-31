1a59ed7337827777c25df2d26439df374be0ee15
Derek Stuckey <djs37549@live.missouristate.edu>
2018-01-31 13:08:49 -0600

    memoization started

--- csc131/more_fib_methods.py
+++ csc131/more_fib_methods.py
@@ -2,18 +2,40 @@ def fib_memoization(n: int, lookup: dict) -> int:
     if n < 1:
         return -1
     # base case
-    if n == 1 or n == 2:
+    elif n == 1 or n == 2:
         lookup[n] = 1
 
-    if lookup.get(n, None) is None:
+    elif lookup.get(n, None) is None:
         lookup[n] = fib_memoization(n - 1, lookup) + fib_memoization(n - 2, lookup)
         return lookup
+
+def fib_tab(n: int) -> int:
+    f = [0] * (n+1)
+
+    #basecase
+    f[1] = 1
+
+    for i in range(2, n + 1):
+        f[i] = f[i - 1] + f[i - 2]
+        return f[n]
+        print(f[n])
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
     my_dictionary = {}
     f = fib_memoization(6, my_dictionary)
     print(f)
+
 
 
 if __name__ == '__main__':
